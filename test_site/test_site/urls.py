933fdcd99dd39306f7bbc48ff3dd04c2c9276f13
jlik2002 <jlik20002@gmail.com>
2021-03-19 17:40:33 +0300

    rename

--- test_site/test_site/urls.py
+++ test_site/test_site/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
 
-from . import views
 
 urlpatterns = [
     path('user/',include('users.urls'))
